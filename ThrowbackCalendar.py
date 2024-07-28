4581664f8837ca46bd852b714a1d530f57d750cf
Gerald Schneider <gerald@schneidr.de>
2024-07-28 10:10:26 +0200

    removed debug output

--- ThrowbackCalendar.py
+++ ThrowbackCalendar.py
@@ -10,7 +10,6 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 app.config.from_pyfile("settings.py")
-print(app.config)
 
 
 def get_locale():
